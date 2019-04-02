1d85f3ab41123a08d56a18077012966562eb8863
alafiAvichai <49117514+alafiAvichai@users.noreply.github.com>
2019-04-02 12:24:25 +0300

    Update accessibilityScript.js

--- accessibilityScript.js
+++ accessibilityScript.js
@@ -132,6 +132,7 @@ var div = document.createElement("div");
 div.setAttribute("id", "divAccessible");
 
 div.style.border = "1px solid #333";
+div.style.backgroundColor = "#fff";
 div.style.visibility = "hidden";
 
 
@@ -155,9 +156,9 @@ allLi += '<li class="accessibileLi"><a class="aTagAccessibility" href="#" id="sm
 allLi += '<li class="accessibileLi"><a class="aTagAccessibility" href="#" id="ContrastAccessibility">ניגודיות גבוהה <i class="fas fa-adjust"></i></a></li>';
 allLi += '<li class="accessibileLi"><a class="aTagAccessibility" href="#" id="ContrastReverseAccessibility">ניגודיות הפוכה <i class="far fa-eye"></i></a></li>';
 allLi += '<li class="accessibileLi"><a class="aTagAccessibility" href="#" id="whitBackroundAccessibility">רקע בהיר <i class="fas fa-chalkboard"></i></a></li>';
-allLi += '<li class="accessibileLi"><a class="aTagAccessibility" href="#" id="boltLinkAccessibility">הדגשת קישורים<i class="fas fa-link"></i></a></li>';
+allLi += '<li class="accessibileLi"><a class="aTagAccessibility" href="#" id="boltLinkAccessibility">הדגשת קישורים <i class="fas fa-link"></i></a></li>';
 allLi += '<li class="accessibileLi"><a class="aTagAccessibility" href="#" id="readFontAccessibility">פונט קריא <i class="fas fa-font"></i></a></li>';
-allLi += '<li class="accessibileLi"><a class="aTagAccessibility" href="#" id="resetAccessibility">איפוס<i class="fas fa-undo-alt"></i></a></li>'
+allLi += '<li class="accessibileLi"><a class="aTagAccessibility" href="#" id="resetAccessibility">איפוס <i class="fas fa-undo-alt"></i></a></li>'
 ul.innerHTML = allLi;
 
 var li = document.getElementsByClassName("accessibileLi");
